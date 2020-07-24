435c9b37caca66f81d13d36a2871864bb5197f68
Jwahir Sundai <jwahirsundai@Jwahirs-Air.fios-router.home>
2020-07-24 02:37:02 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+import './App.css';
+import ButtonAppBar from './components/Header'
+import TitlebarGridList from './components/Cards'
+import MyButton from './components/Footer'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ButtonAppBar />
+        <TitlebarGridList />
+        <MyButton/>
+      </div>
+    );
+  }
+}
+
+export default App;
