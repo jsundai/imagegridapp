c93489b3025f0f507d25dcf1be0ccdd080a46a85
Jwahir S <36107423+jsundai@users.noreply.github.com>
2020-10-20 22:16:10 -0400

    Update tileData.js

--- src/components/tileData.js
+++ src/components/tileData.js
@@ -9,19 +9,22 @@ const tileData = [
     featured: true,
     },
     {
-      img: 'https://uifaces.co/our-content/donated/KtCFjlD4.jpg',
-      title: 'Arthur Stewart',
-      featured: true,
+      img: 'https://images.unsplash.com/photo-1543610892-0b1f7e6d8ac1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+      title: 'Hebe Harwood',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+      title: 'Cobie Morin',
     },
     {
       img: 'https://images.pexels.com/photos/1181519/pexels-photo-1181519.jpeg?auto=compress&cs=tinysrgb&h=350',
       title: 'Jay Chapman',
     },
     {
-      img: 'https://images.unsplash.com/photo-1543610892-0b1f7e6d8ac1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-      title: 'Hebe Harwood',
+      img: 'https://uifaces.co/our-content/donated/KtCFjlD4.jpg',
+      title: 'Arthur Stewart',
+      featured: true,
     },
-    
     {
       img: 'https://uifaces.co/our-content/donated/n4Ngwvi7.jpg',
       title: 'Keanu Hood',
@@ -32,15 +35,11 @@ const tileData = [
     },
     {
       img: 'https://images.pexels.com/photos/1394499/pexels-photo-1394499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      title: 'Reema English',
+      title: 'Reema Daniel',
     },
     {
       img: 'https://images.unsplash.com/photo-1456327102063-fb5054efe647?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=f05c14dd4db49f08a789e6449604c490',
       title: 'Atticus Aldred',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
-      title: 'Cobie Morin',
     },
     {
       img: 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&h=350',
