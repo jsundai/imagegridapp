91d6c42a09e8eb83f8de548eac645d5469ff3705
Jwahir Sundai <36107423+jsundai@users.noreply.github.com>
2023-10-09 13:43:28 -0500

    Update tileData.js

--- src/components/tileData.js
+++ src/components/tileData.js
@@ -21,7 +21,7 @@ const tileData = [
       title: 'Cobie Morin',
     },
     {
-      img: 'https://images.unsplash.com/photo-1544168190-79c17527004f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2488&q=80',
+      img: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2960&q=80',
       title: 'Arthur Stewart',
       featured: true,
     },
